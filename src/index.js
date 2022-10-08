dd47cb4ab770a358c190df4fb414646a0669aeb4
junior batista <juniorlevin878@gmail.com>
2022-10-08 12:46:08 -0300

    prova

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import Prova from '../src/prova/Açai'
 import Signo from '../src/prova/signo' 
-import Sorveteria from '../src/prova/sorveteria' 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Sorveteria/>
+    <Signo/>
   </React.StrictMode>
 );
 
